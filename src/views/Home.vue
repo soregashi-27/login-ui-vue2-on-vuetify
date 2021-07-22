06bc0153946ec1c494ad1c4d2c7b6ffe63858ac4
soregashi-27 <tomo.soul.15@gmail.com>
2021-07-22 23:47:42 +0900

    サインアップコンポーネントを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,84 +1,105 @@
 <template>
   <v-app>
-    <v-content>
+    <v-main>
       <v-container class="fill-height" fluid>
         <v-row alight="center" justify="center">
           <v-col cols="12" sm="8" md="8">
             <v-card class="elevation-12">
               <v-window v-model="step">
-                <v-row>
-                  <v-col cols="12" md="8">
-                    <v-card-text class="mt-12">
-                      <h1
-                        class="text-center display-2 teal--text text--accent-3"
-                      >
-                        Sign in to Soregashi-27
-                      </h1>
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-2 teal--text text--accent-3"
+                        >
+                          Sign in to Soregashi-27
+                        </h1>
 
-                      <div class="text-center" mt-4>
-                        <v-btn class="mx-2" fab color="black" outlined>
-                          <v-icon>fab fa-facebook-f</v-icon>
-                        </v-btn>
-                        <v-btn class="mx-2" fab color="black" outlined>
-                          <v-icon>fab fa-google-plus-g</v-icon>
-                        </v-btn>
-                        <v-btn class="mx-2" fab color="black" outlined>
-                          <v-icon>fab fa-linkedin-in</v-icon>
+                        <div class="text-center" mt-4>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+
+                        <h4 class="text-center mlt-4">
+                          Ensure your email for registration
+                        </h4>
+
+                        <v-form>
+                          <v-text-field
+                            label="Email"
+                            name="email"
+                            prepend-icon="email"
+                            type="text"
+                            color="teal accent-3"
+                          />
+
+                          <v-text-field
+                            id="Password"
+                            label="Password"
+                            name="password"
+                            prepend-icon="lock"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+
+                        <h3 class="text-center mt-3">Forget your password?</h3>
+
+                        <div class="text-center mt-3">
+                          <v-btn rounded color="teal accent-3">SIGN IN</v-btn>
+                        </div>
+                      </v-card-text>
+                    </v-col>
+
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Hello, there!</h1>
+                        <h5 class="text-center">
+                          Enter your personal details <br />
+                          and start journey with us!
+                        </h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined="" dark @click="step++">
+                          SIGN UP
                         </v-btn>
                       </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
 
-                      <h4 class="text-center mlt-4">
-                        Ensure your email for registration
-                      </h4>
-
-                      <v-form>
-                        <v-text-field
-                          label="Email"
-                          name="email"
-                          prepend-icon="email"
-                          type="text"
-                          color="teal accent-3"
-                        />
-
-                        <v-text-field
-                          id="Password"
-                          label="Password"
-                          name="password"
-                          prepend-icon="lock"
-                          type="password"
-                          color="teal accent-3"
-                        />
-                      </v-form>
-
-                      <h3 class="text-center mt-3">Forget your password?</h3>
-
-                      <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3">SIGN IN</v-btn>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Welcome Back!!</h1>
+                        <h5 class="text-center">
+                          To keep connected with us. Please login with your
+                          personel info.
+                        </h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step--">
+                          SIGN IN
+                        </v-btn>
                       </div>
-                    </v-card-text>
-                  </v-col>
-
-                  <v-col cols="12" md="4" class="teal accent-3">
-                    <v-card-text class="white--text mt-12">
-                      <h1 class="text-center display-1">Hello, there!</h1>
-                      <h5 class="text-center">
-                        Enter your personal details <br />
-                        and start journey with us!
-                      </h5>
-                    </v-card-text>
-                    <div class="text-center">
-                      <v-btn rounded outlined="" dark @click="step++">
-                        SIGN UP
-                      </v-btn>
-                    </div>
-                  </v-col>
-                </v-row>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
               </v-window>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
